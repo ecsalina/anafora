63f7896e8d511e99bdfcb826739cdeacd28add1c
root <ecsalina@gmail.com>
2020-08-10 19:58:45 +0000

    changed some auth settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -22,9 +22,6 @@ MANAGERS = ADMINS
 #}
 DATABASES = {'default':{}}
 
-# DEBUG
-ALLOWED_HOSTS = ["172.104.17.152"]
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
